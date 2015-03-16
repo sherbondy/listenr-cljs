28619c6075db3c95e8b1475cebc982b1dd50236f
Ethan Sherbondy <sherbondye@gmail.com>
2015-03-16 16:19:58 -0400

    initial commit.

---
+++ dev_src/listenr_cljs/dev.cljs
@@ -0,0 +1,10 @@
+(ns listenr-cljs.dev
+    (:require
+     [listenr-cljs.core]
+     [figwheel.client :as fw]))
+
+(fw/start {
+  :websocket-url "ws://localhost:3449/figwheel-ws"
+  :on-jsload (fn []
+               ;; (stop-and-start-my app)
+               )})
